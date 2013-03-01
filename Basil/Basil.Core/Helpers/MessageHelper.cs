e6eacd33a77eee5ff8c4563aedcfcddb63af59bd
Evan Cummings <emcummings@gmail.com>
2013-02-28 22:18:23 -0500

    Added MessageHelper class to prep (maybe) for variable Render overrides

---
+++ Basil/Basil.Core/Helpers/MessageHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Basil.Enums;
+using Basil.Settings;
+
+namespace Basil.Helpers
+{
+    public static class MessageHelper
+    {
+
+        public static string GetMessage(RequiredTypes type, BasilSettings settings)
+        {
+
+            if (settings == null) return String.Empty;
+
+            switch (type)
+            {
+                case RequiredTypes.Text:
+                    return settings.RequiredFieldValidation.Message;                   
+
+                case RequiredTypes.Zip:
+                    return settings.ZipValidation.Message;
+                    
+
+                case RequiredTypes.Phone:
+                    return settings.PhoneValidation.Message;
+                    
+
+                case RequiredTypes.Email:
+                    return settings.EmailValidation.Message;
+                    
+
+                case RequiredTypes.SSN:
+                    return settings.SocialSecurityNumberValidation.Message;
+                    
+
+                case RequiredTypes.Number:
+                    return settings.NumericEntryValidation.Message;
+                    
+
+                case RequiredTypes.Date:
+                    return settings.DateValidation.Message;                    
+            }
+
+            return null;
+        }
+    }
+}
